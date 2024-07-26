119a8fc36a6318c48e59f6f6aae5c3f4511b0350
gooncalvesgaby <gabrielle.goncalves@outlook.com.br>
2024-07-25 22:20:41 -0300

    alteração folha de estilos css

--- index.js
+++ index.js
@@ -1,13 +1,2 @@
-let arrayList = []
 
-
-function addtolist() {
-    let inputAdicionar = document.getElementById("add-input")
-
-    if(inputAdicionar.value == ""){
-        alert("Digite um valor valido")
-    } else {
-        arrayList.push(inputAdicionar.value)
-    }    
-}
 
