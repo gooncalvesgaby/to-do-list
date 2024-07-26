710ed376b5d5eb91118224c5c61736da6c8c335b
gooncalvesgaby <gabrielle.goncalves@outlook.com.br>
2024-07-25 22:47:55 -0300

    inicio js

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+const inputTask = document.getElementById("add-input");
+const taskList = document.getElementById("add-list");
+const list = document.getElementById("list");
 
+function criaLi() {
+    const li = document.createElement('li');
+    return li;
+}
+
+function createTask(textoInput) {
+    const li = criaLi();
+    li.innerHTML = textoInput;
+};
+
+taskList.addEventListener('click', function() {
+    if (!inputTask.value) return;
+    createTask(inputTask.value);
+});
 
