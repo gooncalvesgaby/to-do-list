2e0ae254e22071cd9f4189861a9c03254529b535
gooncalvesgaby <gabrielle.goncalves@outlook.com.br>
2024-07-25 23:15:21 -0300

    criação de funções

--- index.js
+++ index.js
@@ -2,14 +2,37 @@ const inputTask = document.getElementById("add-input");
 const taskList = document.getElementById("add-list");
 const list = document.getElementById("list");
 
-function criaLi() {
+function createLi() {
     const li = document.createElement('li');
     return li;
 }
 
+inputTask.addEventListener('keypress', function(e) {
+    if (e.keyCode === 13) {
+        if (!inputTask.value) return;
+        createTask(inputTask.value);
+    }
+});
+
+function clearInput() {
+    inputTask.value = '';
+    inputTask.focus();
+}
+
+function createButtonClear(li) {
+    li.innerText += ' ';
+    const buttonClear = document.createElement('button');
+    buttonClear.innerText = 'Apagar';
+    li.appendChild(buttonClear);
+
+}
+
 function createTask(textoInput) {
-    const li = criaLi();
+    const li = createLi();
     li.innerHTML = textoInput;
+    list.appendChild(li);
+    clearInput();
+    createButtonClear(li);
 };
 
 taskList.addEventListener('click', function() {
